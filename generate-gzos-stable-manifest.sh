aca78a9c9d0df17086cc8ed1bd4e64c91ef87832
James Kung <kong1191@gmail.com>
2018-05-05 14:48:55 +0800

    [gzos] gzos stable development environment
    Change-Id: Ia7f3f774627d64581d9700fcd10220ab96c4802b

---
+++ generate-gzos-stable-manifest.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright 2018 Open Trust Group
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+
+function usage {
+  cat <<END
+usage: $0 <out_manifest_file>
+
+Generate gzos stable manifest from current development environment.
+END
+}
+
+if [[ $# -ne 1 ]]; then
+  usage
+  exit 1
+fi
+
+dev_root=`jiri -show-root`
+
+jiri snapshot /tmp/gzos_stable.tmp
+sed 's/remotebranch="gzos" revision="[0-9a-z]\+"/remotebranch="gzos"/' /tmp/gzos_stable.tmp > ${1}
+
+echo "Manifest generated."
+echo "Please compare and replace the content of ${dev_root}/manifest/gzos_stable with ${1} if needed."
+echo "If gzos_stable was changed, please submit the changes to gerrit for review."
